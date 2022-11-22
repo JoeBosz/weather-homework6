963666b4b81c8a5f54b6925e2363c653c235289d
JoeBosz <joeboz2213@gmail.com>
2022-11-22 00:22:06 -0600

    Added api

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var  searchBtn=document.getElementById("searchBtn") 
+var citySearch=document.getElementById("citySearch")
+var weatherSource= 'https://api.github.com/https://api.openweathermap.org/data/2.5/weather?q=%22St.%20Louis&22%limit=18&appid=e353137ebeb01b211cb86198e83815df'
 
-searchBtn.addEventListener('click', function() {
+searchBtn.addEventListener('click', function() { 
+fetch(weatherSource)
+.then((response) => response.json())
+.then((data => console.log(data))) 
     console.log("click")
+    console.log(citySearch.value)
+   
 })
+
+
+
