21672417f1b8b382d854d1b513f2047ab6cb6bae
JoeBosz <joeboz2213@gmail.com>
2022-11-26 15:23:27 -0600

    api working

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var  searchBtn=document.getElementById("searchBtn") 
 var citySearch=document.getElementById("citySearch")
-var weatherSource= 'https://api.github.com/https://api.openweathermap.org/data/2.5/weather?q=%22St.%20Louis&22%limit=18&appid=e353137ebeb01b211cb86198e83815df'
+var weatherSource= 'https://api.openweathermap.org/data/2.5/forecast?lat=38.6319657&lon=-90.2428756&appid=e353137ebeb01b211cb86198e83815df&units=imperial'
 
 searchBtn.addEventListener('click', function() { 
 fetch(weatherSource)
 .then((response) => response.json())
 .then((data => console.log(data))) 
     console.log("click")
-    console.log(citySearch.value)
+   // console.log(citySearch.value)
    
 })
 
